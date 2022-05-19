3fcd27e9e0ca138eed46cd04024d7e3f5c4caf37
margiezhang <margiezhang@tencent.com>
2022-05-19 11:13:30 +0800

    feat: dropdown下拉

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="tdesign-demo-dropdown">
+    <t-dropdown :options="options" :min-column-width="112" @click="clickHandler">
+      <div class="tdesign-demo-dropdown-trigger">
+        <t-button variant="text">
+          <span class="tdesign-demo-dropdown__text"> 下拉菜单 <t-icon name="chevron-down" size="16" /> </span>
+        </t-button>
+      </div>
+    </t-dropdown>
+    <br/>
+    <t-dropdown :min-column-width="88" trigger="click" @click="handleClick">
+      <t-button variant="text"> 下拉菜单 </t-button>
+      <template #dropdown>
+        <t-dropdown-menu>
+          <t-dropdown-item :value="1"> 操作一 </t-dropdown-item>
+          <t-dropdown-item :value="2"> 操作二 </t-dropdown-item>
+        </t-dropdown-menu>
+      </template>
+    </t-dropdown>
+  </div>
 </template>
+<script setup>
+import { MessagePlugin } from 'tdesign-vue-next';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const options = [
+  {
+    content: '操作一',
+    value: 1,
+  },
+  {
+    content: '操作二',
+    value: 2,
+  },
+  {
+    content: '操作三',
+    value: 3,
+  },
+  {
+    content: '操作四',
+    value: 4,
+  },
+];
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const clickHandler = (data) => {
+  console.log('clickHandler', data);
+  MessagePlugin.success(`选中【${data.content}】`);
+};
+
+
+const handleClick = (data, { e }) => {
+  console.log('handleClick', data);
+  e.preventDefault();
+};
 </script>
+<style scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.tdesign-demo-dropdown__text {
+  display: inline-flex;
+}
+.t-icon {
+  margin-left: 8px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import TDesign from 'tdesign-vue-next';
+// 引入组件库全局样式资源
+import 'tdesign-vue-next/es/style/index.css';
 
-createApp(App).mount('#app')
+
+createApp(App).use(TDesign).mount('#app')
